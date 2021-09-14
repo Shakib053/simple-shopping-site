a1de6d03172dae7ae200c5efc4b40291823fc7e6
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-09-14 22:25:11 +0600

    update

--- js/app.js
+++ js/app.js
@@ -14,19 +14,21 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     // div.classList.add("product");
+
     div.innerHTML = `<div class="single-product mx-2 rounded" style="height: 36rem; background-color:#F9E3E6;" >
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h5>Rating:<span class="text-danger"> ${product.rating.rate} </span></h5>
-      <h5>Reviewed By <span class="text-danger">${product.rating.count}</span>  People</h5>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to Cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    <div>
+  <img class="product-image" src=${image}></img>
+    </div>
+    <h3>${product.title}</h3>
+    <p>Category: ${product.category}</p>
+    <h5>Rating:<span class="text-danger"> ${product.rating.rate} </span></h5>
+    <h5>Reviewed By <span class="text-danger">${product.rating.count}</span>  People</h5>
+    <h2>Price: $ ${product.price}</h2>
+    <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary ">Add to Cart</button>
+    <button id="details-btn" class="btn btn-danger">Details</button></div>
+    `;
     document.getElementById("all-products").appendChild(div);
+
   }
 };
 let count = 0;
@@ -91,4 +93,5 @@ const updateTotal = () => {
   // grandTotal = grandTotal.toFixed(2);
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
 loadProducts();
